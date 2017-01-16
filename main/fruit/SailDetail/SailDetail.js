f0343cf03cbcac6a367bf1edff79563792eef06d
BowenTH <1578779013@qq.com>
2017-01-16 23:58:28 +0800

    更新商户详情界面

--- main/fruit/SailDetail/SailDetail.js
+++ main/fruit/SailDetail/SailDetail.js
@@ -2,28 +2,40 @@
 mui.plusReady(function(){
 //		var flagMenu=[{button:'点菜',id:'foodMenu.html'},{button:'评价',id:'appraise.html'}];
 		var flagMenu='点菜';
+		mui.back=function(){
+			plus.webview.currentWebview().hide();
+			flagMenu='点菜';
+		}
+		
 		mui('.mui-table-view').on('tap','.mui-table-view-cell',function(){
 			//mui.toast(this.getElementsByClassName('tab')[0].innerHTML);
+			if(localStorage.getItem('SailDetailBack')=='1')//判断是否是退出后，再进来的要初始化flagMenu
+			{
+				flagMenu='点菜';localStorage.setItem('SailDetailBack','0');
+			}
 		var tabFlag=this.getElementsByClassName('tab')[0].innerHTML;
-		console.log(tabFlag.toString());
 		
-//		if(flagMenu[0].button==tabFlag)return;
-//		plus.webview.getWebviewById(flagMenu[0].id).hide();
-//		plus.webview.getWebviewById(flagMenu[1],id).show();
-//		
-		flagMenu=tabFlag;
-		
-		if (tabFlag=='点菜'&&flagMenu)return;
+		if (tabFlag==flagMenu)return;
 		if(tabFlag=='点菜'){
-			mui.toast('menu');
 			plus.webview.getWebviewById('appraise.html').hide();
 			plus.webview.getWebviewById('foodMenu.html').show();
 		} else{
-			mui.toast('appraise');
 			plus.webview.getWebviewById('foodMenu.html').hide();
 			plus.webview.getWebviewById('appraise.html').show();
-			flagMenu=0;
 		}
+		flagMenu=tabFlag;
+		
+//		if (tabFlag=='点菜'&&flagMenu)return;
+//		if(tabFlag=='点菜'){
+//			mui.toast('menu');
+//			plus.webview.getWebviewById('appraise.html').hide();
+//			plus.webview.getWebviewById('foodMenu.html').show();
+//		} else{
+//			mui.toast('appraise');
+//			plus.webview.getWebviewById('foodMenu.html').hide();
+//			plus.webview.getWebviewById('appraise.html').show();
+//			flagMenu=0;
+//		}
 	});
 });	
 
